59d2635a1661d11e4ba9ac0e730f883f0390e8d2
Sunit Deshpande <sunitdeshpande1234@gmail.com>
2016-12-18 21:38:07 +0530

    FileFix - Added new files

---
+++ js/custom.js
@@ -0,0 +1,140 @@
+jQuery(document).ready(function(){ 
+	
+	/* ---------------------------------------------------------------------- */
+	/*	Custom Functions
+	/* ---------------------------------------------------------------------- */
+
+	// Logo
+	var $logo 	= $('#logo');
+
+    if (location.href.indexOf("#") != -1) {
+        $logo.show();
+    }
+	// Show logo 
+	$('.menu .tabs a').click(function() {
+	  $logo.fadeIn('slow');
+	});
+	// Hide logo
+	$('.tab-profile').click(function() {
+	  $logo.fadeOut('slow');
+	});	
+
+	/* ---------------------------------------------------------------------- */
+	/*	About
+	/* ---------------------------------------------------------------------- */
+	
+	// Profile Photo Slider
+	 $(".photo-inner ul").carouFredSel({
+        direction           : "left",
+ 	    circular: true,
+        auto    			: true,
+        scroll 			: {
+            items           : 1,
+            fx 				: 'crossfade',
+            duration        : 1500,                        
+            wipe    		: true
+        },
+	    swipe: {
+	        onTouch: true
+	    },
+        items: {
+            width: 153
+        }           
+    });
+	 
+	/* ---------------------------------------------------------------------- */
+	/*	Menu
+	/* ---------------------------------------------------------------------- */
+	
+	// Needed variables
+	var $content 		= $("#content");
+	
+	// Run easytabs
+  	$content.easytabs({
+	  animate			: true,
+	  updateHash		: false,
+	  transitionIn		:'slideDown',
+	  transitionOut		:'slideUp',
+	  animationSpeed	:600,
+	  tabs				:".tmenu",
+	  tabActiveClass	:'active',
+	});
+
+	
+	// Hover menu effect
+	$content.find('.tabs li a').hover(
+		function() {
+			$(this).stop().animate({ marginTop: "-7px" }, 200);
+		},function(){
+			$(this).stop().animate({ marginTop: "0px" }, 300);
+		}
+	);
+
+	// Menu Navigation
+	 $(".menu .tabs").carouFredSel({
+        responsive          : true,
+        direction           : "left",
+ 	    circular: false,
+    	infinite: false,
+        pagination  		: "#menu-controls",  
+        auto    			: false,
+        scroll 			: {
+            items           : 1,
+            duration        : 300,                        
+            wipe    : true
+        },
+		prev	: {	
+			button	: "#menu-prev",
+			key		: "right"
+		},
+		next	: { 
+			button	: "#menu-next",
+			key		: "left"
+		},
+	    swipe: {
+	        onTouch: true
+	    },
+        items: {
+            width: 140,
+            visible: {
+              min: 2,
+              max: 5
+            }
+        }           
+    });
+
+	/* --------------------------------------------- */
+	/* Filtering ------------------------------------*/
+	/* ----------------------------------------------*/
+	var options = {
+   animationDuration: 0.5, //in seconds
+   filter: 'all', //Initial filter
+   callbacks: { 
+      onFilteringStart: function() { },
+      onFilteringEnd: function() { },
+      onShufflingStart: function() { },
+      onShufflingEnd: function() { },
+      onSortingStart: function() { },
+      onSortingEnd: function() { }
+   },
+   delay: 0, //Transition delay in ms
+   delayMode: 'progressive', //'progressive' or 'alternate'
+   easing: 'ease-out',
+   filterOutCss: { //Filtering out animation
+      opacity: 0,
+      transform: 'scale(0.5)'
+   },
+   filterInCss: { //Filtering in animation
+      opacity: 0,
+      transform: 'scale(1)'
+   },
+   layout: 'sameSize', //See layouts
+   selector: '.filtr-container',
+   setupControls: true 
+} 
+//You can override any of these options and then call...
+var filterizd = $('.filtr-container').filterizr(options);
+//If you have already instantiated your Filterizr then call...
+filterizd.filterizr('setOptions', options);
+
+});	
