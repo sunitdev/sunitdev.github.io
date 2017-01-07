356aff20a379ec4b3022fa53d2702b6f9292a11c
Sunit Deshpande <sunitdeshpande1234@gmail.com>
2017-01-07 12:10:47 +0530

    Added analystics

--- js/custom.js
+++ js/custom.js
@@ -19,28 +19,6 @@ jQuery(document).ready(function(){
 	  $logo.fadeOut('slow');
 	});	
 
-	/* ---------------------------------------------------------------------- */
-	/*	About
-	/* ---------------------------------------------------------------------- */
-	
-	// Profile Photo Slider
-	 $(".photo-inner ul").carouFredSel({
-        direction           : "left",
- 	    circular: true,
-        auto    			: true,
-        scroll 			: {
-            items           : 1,
-            fx 				: 'crossfade',
-            duration        : 1500,                        
-            wipe    		: true
-        },
-	    swipe: {
-	        onTouch: true
-	    },
-        items: {
-            width: 153
-        }           
-    });
 	 
 	/* ---------------------------------------------------------------------- */
 	/*	Menu
@@ -102,39 +80,4 @@ jQuery(document).ready(function(){
             }
         }           
     });
-
-	/* --------------------------------------------- */
-	/* Filtering ------------------------------------*/
-	/* ----------------------------------------------*/
-	var options = {
-   animationDuration: 0.5, //in seconds
-   filter: 'all', //Initial filter
-   callbacks: { 
-      onFilteringStart: function() { },
-      onFilteringEnd: function() { },
-      onShufflingStart: function() { },
-      onShufflingEnd: function() { },
-      onSortingStart: function() { },
-      onSortingEnd: function() { }
-   },
-   delay: 0, //Transition delay in ms
-   delayMode: 'progressive', //'progressive' or 'alternate'
-   easing: 'ease-out',
-   filterOutCss: { //Filtering out animation
-      opacity: 0,
-      transform: 'scale(0.5)'
-   },
-   filterInCss: { //Filtering in animation
-      opacity: 0,
-      transform: 'scale(1)'
-   },
-   layout: 'sameSize', //See layouts
-   selector: '.filtr-container',
-   setupControls: true 
-} 
-//You can override any of these options and then call...
-var filterizd = $('.filtr-container').filterizr(options);
-//If you have already instantiated your Filterizr then call...
-filterizd.filterizr('setOptions', options);
-
 });	
