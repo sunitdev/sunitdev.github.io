d30976d448acba979ef8a62af36d1185870d705d
Sunit Deshpande <sunitdeshpande1234@gmail.com>
2022-04-17 17:50:07 +0200

    [Content - Adding resume link]

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -4,126 +4,138 @@ import styled from 'styled-components';
 
 import * as ReactGA from 'react-ga';
 
-import { HomePageTemplate } from '../../templates/HomePageTemplate';
+import {HomePageTemplate} from '../../templates/HomePageTemplate';
 
-import { IconButton } from '../../atoms/IconButton';
-import { HeadingText } from '../../atoms/HeadingText';
-import { FooterText } from '../../atoms/FooterText';
-import { EyeBar } from '../../organisms/EyeBar';
-import { IconBar } from '../../organisms/IconBar';
+import {IconButton} from '../../atoms/IconButton';
+import {HeadingText} from '../../atoms/HeadingText';
+import {FooterText} from '../../atoms/FooterText';
+import {EyeBar} from '../../organisms/EyeBar';
+import {IconBar} from '../../organisms/IconBar';
 
-const GithubLogo  = require('../../../assets/images/github_logo.jpg');
-const ProjectIcon  = require('../../../assets/images/project_icon.png');
+const GithubLogo = require('../../../assets/images/github_logo.jpg');
+const ProjectIcon = require('../../../assets/images/project_icon.png');
 const LinkedInLogo = require('../../../assets/images/linked_in_logo.png');
+const ResumeLogo = require('../../../assets/images/cv-icon.png');
 
 const NameText = styled(HeadingText)`
-    text-align: center;
+  text-align: center;
 
-    margin-bottom: 10px;
+  margin-bottom: 10px;
 `;
 
 const BioText = styled.p`
-    text-align: center;
-    margin-top: 0;
+  text-align: center;
+  margin-top: 0;
 
-    color: ${props => props.theme.colors.secondaryTextForeground};
+  color: ${props => props.theme.colors.secondaryTextForeground};
 
-    /* Smartphones (portrait and landscape) ----------- */
-    @media only screen and (min-device-width : 320px) and (max-device-width : 480px) {
-        font-size: 2em;
-    }
+  /* Smartphones (portrait and landscape) ----------- */
+  @media only screen and (min-device-width: 320px) and (max-device-width: 480px) {
+    font-size: 2em;
+  }
 `;
 
 const HomePage = (props: any) => {
 
-    function handelGithubIconClicked(){
+  function handelGithubIconClicked() {
 
-        // Google analytics
-        ReactGA.event({
-            category: 'User',
-            action: 'Github Profile Icon Clicked'
-        })
+    // Google analytics
+    ReactGA.event({
+      category: 'User',
+      action: 'Github Profile Icon Clicked'
+    })
 
-        // Open github profile
-        window.open('https://github.com/sunitdev', '_blank');
-    }
+    // Open github profile
+    window.open('https://github.com/sunitdev', '_blank');
+  }
 
-    function handelProjectIconClicked(){
+  function handelProjectIconClicked() {
 
-        // Google analytics
-        ReactGA.event({
-            category: 'User',
-            action: 'Project Icon Clicked'
-        })
+    // Google analytics
+    ReactGA.event({
+      category: 'User',
+      action: 'Project Icon Clicked'
+    })
 
-        // Open project page
-        props.history.push('/projects/');
-    }
+    // Open project page
+    props.history.push('/projects/');
+  }
 
-    function handelOpenSourceIconClicked(){
+  function handelOpenSourceIconClicked() {
 
-        // Google analytics
-        ReactGA.event({
-            category: 'User',
-            action: 'Open Source Icon Clicked'
-        })
+    // Google analytics
+    ReactGA.event({
+      category: 'User',
+      action: 'Open Source Icon Clicked'
+    })
 
-        // Open project page
-        props.history.push('/open-source/');
-    }
+    // Open project page
+    props.history.push('/open-source/');
+  }
 
-    function handelLinkedInIconClicked() {
+  function handelLinkedInIconClicked() {
 
-        // Google analytics
-        ReactGA.event({
-            category: 'User',
-            action: 'LinkedIn Profile Icon Clicked'
-        })
+    // Google analytics
+    ReactGA.event({
+      category: 'User',
+      action: 'LinkedIn Profile Icon Clicked'
+    })
 
-        // Open linkedin profile
-        window.open('https://www.linkedin.com/in/sunitdeshpande/', '_blank');
-    }
+    // Open linkedin profile
+    window.open('https://www.linkedin.com/in/sunitdeshpande/', '_blank');
+  }
 
-//                      <IconButton
-//                         src={OpenSourceLogo}
-//                         text={'Open Source'}
-//                         onClick={handelOpenSourceIconClicked}/>
-    return (
-        <HomePageTemplate
-            eyeBar={
-                <EyeBar/>
-            }
+  function handleResumeIconClicked() {
+    // Google analytics
+    ReactGA.event({
+      category: 'User',
+      action: 'Resume Icon Clicked'
+    })
 
-            nameAndBio = {
-                <React.Fragment>
-                    <NameText>Sunit Deshpande</NameText>
-                    <BioText>Full-Stack Developer @Amazon</BioText>
-                </React.Fragment>
-            }
+    // Open linkedin profile
+    window.open('https://sunitdev.github.io/resume/', '_blank');
+  }
 
-            iconBar = {
-                <IconBar>
-                    <IconButton
-                        src={GithubLogo}
-                        text={'Github'}
-                        onClick={handelGithubIconClicked}/>
-                    <IconButton
-                        src={ProjectIcon}
-                        text={'Projects'}
-                        onClick={handelProjectIconClicked}/>
-                    <IconButton
-                        src={LinkedInLogo}
-                        text={'LinkedIn'}
-                        onClick={handelLinkedInIconClicked}/>
-                </IconBar>
-            }
+  return (
+    <HomePageTemplate
+      eyeBar={
+        <EyeBar/>
+      }
 
-            footer={
-                <FooterText/>
-            }
-            />
-    );
+      nameAndBio={
+        <React.Fragment>
+          <NameText>Sunit Deshpande</NameText>
+          <BioText>Full-Stack Developer @Amazon</BioText>
+        </React.Fragment>
+      }
+
+      iconBar={
+        <IconBar>
+          <IconButton
+            src={GithubLogo}
+            text={'Github'}
+            onClick={handelGithubIconClicked}/>
+          <IconButton
+            src={ProjectIcon}
+            text={'Projects'}
+            onClick={handelProjectIconClicked}/>
+          <IconButton
+            src={LinkedInLogo}
+            text={'LinkedIn'}
+            onClick={handelLinkedInIconClicked}/>
+          <IconButton
+            src={ResumeLogo}
+            text={'Resume'}
+            onClick={handleResumeIconClicked}/>
+        </IconBar>
+      }
+
+      footer={
+        <FooterText/>
+      }
+    />
+  );
 
 }
 
-export { HomePage }
+export {HomePage}
